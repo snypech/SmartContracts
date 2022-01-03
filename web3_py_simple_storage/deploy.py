8c9d7b5dc951f8dcd905c31ca3b2b34c5a7b6016
snypech <sneyder.yepes@gmail.com>
2022-01-02 22:29:27 -0500

    web3, contract building, signind, deployment and confirmation

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,5 +1,10 @@
 from solcx import compile_standard
 import json
+from web3 import Web3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # install solcx 0.6.0 install_solc("0.6.0")
 
@@ -20,6 +25,50 @@ compiled_sol = compile_standard(
     },
     solc_version="0.6.0",
 )
-print(compiled_sol)
 with open("compiled_sol.json", "w") as file:
     json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
+
+w3 = Web3(Web3.HTTPProvider(os.getenv("RPC_URL")))
+
+# For connecting to ganache
+chain_id = 1337
+my_address = os.getenv("MY_ADDRESS")
+private_key = os.getenv("PRIVATE_KEY")
+# Create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+
+# Steps
+# 1 . Build transaction
+# 2 . Sign transaction
+# 3 . Send transaction
+
+# Submit the transaction that deploys the contract
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+# Sign the transaction
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+print("Deploying Contract!")
+# Send it!
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+# Wait for the transaction to be mined, and get the transaction receipt
+print("Waiting for transaction to finish...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
