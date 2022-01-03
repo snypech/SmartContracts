61f7c7a73ca126c5b0cbb669b0eff9b42601db81
snypech <sneyder.yepes@gmail.com>
2022-01-02 22:43:20 -0500

    interact with contract

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -72,3 +72,26 @@ tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 print("Waiting for transaction to finish...")
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
+
+
+# Working with deployed Contracts
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+print(f"Initial Stored Value {simple_storage.functions.getFavoriteNumber().call()}")
+
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+
+signed_store_tx = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+tx_hash = w3.eth.send_raw_transaction(signed_store_tx.rawTransaction)
+print("Updating stored Value...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Done! store transaction completed")
+print(f"New Stored Value {simple_storage.functions.getFavoriteNumber().call()}")
