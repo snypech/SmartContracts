ad149b39320df9ffb4d1e64265112d5c018f755c
snypech <sneyder.yepes@gmail.com>
2022-01-03 20:33:32 -0500

    contract deployment, read, simple_storage using brownie

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,12 +1,20 @@
-from brownie import accounts
+from types import SimpleNamespace
+from brownie import accounts, SimpleStorage
 
 
 def deploy_simple_storage():
     # work with ganachicli accounts
-    # account = accounts[0]
+    account = accounts[0]
     # print(account)
-    account = accounts.load("secondacc-brownie")
-    print(account)
+    # account = accounts.load("secondacc-brownie")
+    # print(account)
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.getFavoriteNumber()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    stored_value = simple_storage.getFavoriteNumber()
+    print(stored_value)
 
 
 def main():
