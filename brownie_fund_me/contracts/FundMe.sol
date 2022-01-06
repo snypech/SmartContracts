70037811c9be4985aaa5618764fe7466b145fa43
snypech <sneyder.yepes@gmail.com>
2022-01-06 16:20:27 -0500

    add getentrancefee to contact, create fund and withdraw script

--- brownie_fund_me/contracts/FundMe.sol
+++ brownie_fund_me/contracts/FundMe.sol
@@ -48,6 +48,14 @@ contract FundMe {
         return ethAmountInUsd;
     }
 
+    function getEntranceFee() public view returns (uint256) {
+        // mimimumUSD
+        uint256 mimimumUSD = 50 * 10**18;
+        uint256 price = getPrice();
+        uint256 precision = 1 * 10**18;
+        return (mimimumUSD * precision) / price;
+    }
+
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
