c86c3d36edd623c329536a360123a1708d867829
snypech <sneyder.yepes@gmail.com>
2022-01-17 17:18:20 -0500

    get borrowable data function, convert eth to dai, borrow some dai

--- aave_brownie/scripts/aave_borrow.py
+++ aave_brownie/scripts/aave_borrow.py
@@ -22,6 +22,55 @@ def main():
     )
     tx.wait(1)
     print("Deposited")
+    # How much can I borrow
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+    print("let's borrow")
+    # DAI in terms of ETH
+    dai_eth_price = get_asset_price(
+        config["networks"][network.show_active()]["dai_eth_price_feed"]
+    )
+    amount_dai_to_borrow = (1 / dai_eth_price) * (borrowable_eth * 0.95)
+    # borrowable_eth -> borrowable_dai * 95%\
+    print(f"We are going to borrow {amount_dai_to_borrow} DAI")
+    # Now we will borrow!
+    dai_address = config["networks"][network.show_active()]["dai_token"]
+    borrow_tx = lending_pool.borrow(
+        dai_address,
+        Web3.toWei(amount_dai_to_borrow, "ether"),
+        1,
+        0,
+        account.address,
+        {"from": account},
+    )
+    borrow_tx.wait(1)
+    print("We borrowed some DAI!")
+    get_borrowable_data(lending_pool, account)
+
+
+def get_asset_price(price_feed_address):
+    dai_eth_price_feed = interface.AggregatorV3Interface(price_feed_address)
+    latest_price = dai_eth_price_feed.latestRoundData()[1]
+    converted_latest_price = Web3.fromWei(latest_price, "ether")
+    print(f"The DAI/ETH price is {converted_latest_price}")
+    return float(converted_latest_price)
+
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    print(f"You have {total_collateral_eth} worth of ETH deposited.")
+    print(f"You have {total_debt_eth} worth of ETH borrowed.")
+    print(f"You can borrow {available_borrow_eth} worth of ETH.")
+    return (float(available_borrow_eth), float(total_debt_eth))
 
 
 def approve_erc20(amount, spender, erc20_address, account):
