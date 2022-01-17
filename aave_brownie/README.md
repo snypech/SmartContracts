f82b94d0753c53c8d62234ffc822f4409c69cc98
snypech <sneyder.yepes@gmail.com>
2022-01-16 23:15:29 -0500

    aave first commit, get weth from weth interface and address in kovan testnet

---
+++ aave_brownie/README.md
@@ -0,0 +1,15 @@
+1. Swap eth for weth
+
+Use weth interface to get token contract
+
+2. Deposit weth into aave
+3. Borrow some asset with ETH collateral
+    1. Sell that borrowed asset (Short selling)
+4. Repay everything back
+
+
+#Testing:
+
+Integration test: kovan
+
+Unit Test: Mainnet fork (Use mokcs when working with oracles)
---
+++ aave_brownie/scripts/get_weth.py
@@ -0,0 +1,14 @@
+from scripts.helpful_scripts import get_account
+from brownie import interface, config, network
+
+
+def main():
+    get_weth()
+
+
+def get_weth():
+    account = get_account()
+    weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
+    tx = weth.deposit({"from": account, "value": 0.01 * 10 ** 18})
+    print("Received 0.01 weth")
+    return tx
