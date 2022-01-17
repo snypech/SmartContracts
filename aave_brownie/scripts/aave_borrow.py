50bb28043cadfc72be541ffb7579d168bf0b916d
snypech <sneyder.yepes@gmail.com>
2022-01-17 16:06:57 -0500

    deposit weth into aave

--- aave_brownie/scripts/aave_borrow.py
+++ aave_brownie/scripts/aave_borrow.py
@@ -3,7 +3,7 @@ from scripts.get_weth import get_weth
 from brownie import config, network, interface
 from web3 import Web3
 
-amount = Web3.toWei(0.1, "ether")
+amount = Web3.toWei(0.01, "ether")
 
 
 def main():
@@ -15,6 +15,13 @@ def main():
     lending_pool = get_lending_pool()
     # Approbe ERC20
     approve_erc20(amount, lending_pool.address, erc20_address, account)
+    # Deposit weth into aave deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode (deprecated))
+    print("Depositing...")
+    tx = lending_pool.deposit(
+        erc20_address, amount, account.address, 0, {"from": account}
+    )
+    tx.wait(1)
+    print("Deposited")
 
 
 def approve_erc20(amount, spender, erc20_address, account):
